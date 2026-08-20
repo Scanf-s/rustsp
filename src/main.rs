31359ccebd95103efbc57ffe349241a1eec19c65
sullung2yo <sullung2yo@gmail.com>
2026-08-20 22:47:18 +0900

    feat: Test tcp connection through TcpStream with OPTION method to MediaMTX
    Signed-off-by: sullung2yo <sullung2yo@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,42 @@ mod sdp;
 mod transport;
 mod rtp;
 
-fn main() {
+use std::io::{prelude::*, BufReader};
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
     // Week 1: start here. Open a TcpStream to localhost:8554
     // and send an OPTIONS request.
-    println!("rustsp: not implemented yet");
+
+    // Open the TCP connection to the host and port (RTSP needs TCP connection first)
+    let mut stream = TcpStream::connect("127.0.0.1:8554")?;
+    println!("Successfully connected to the server");
+
+    // Send OPTIONS request
+    // https://datatracker.ietf.org/doc/html/rfc2326#section-10.1
+    let request = "OPTIONS rtsp://127.0.0.1:8554 RTSP/1.0\r\n\
+                   CSeq: 1\r\n\
+                   \r\n\
+                   ";
+    println!("REQUEST: {:?}", request);
+
+    // Send the raw bytes over the TCP stream
+    stream.write_all(request.as_bytes())?;
+    stream.flush()?;
+
+    // Read the server's response headers
+    let mut reader = BufReader::new(&stream);
+    let mut response = String::new();
+    reader.read_line(&mut response)?;
+    println!("Status Line: {}", response.trim());
+    loop {
+        let mut line = String::new();
+        reader.read_line(&mut line)?;
+        if line == "\r\n" || line.is_empty() {
+            break; // End of headers reached
+        }
+        println!("Header: {}", line.trim());
+    }
+
+    Ok(())
 }
